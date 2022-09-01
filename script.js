8342938c9589c2ff47a479e72b262752fcb3a44f
Recriando Games <gabriel.mattos17@hotmail.com>
2022-09-01 14:58:24 -0300

    Final updates

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
-const textArray = ["programmer", "full stack", "software engineer", "brother"];
+const textArray = ["programmer", "full stack", "software engineer", "brother"]; //Words list
 const typingDelay = 200, erasingDelay = 100, newTextDelay = 2000;
 let textArrayIndex = 0, charIndex = 0;
 
-function type()
+function writeText()
 {
     if (charIndex < textArray[textArrayIndex].length) 
     {
         if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
         typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
         charIndex++;
-        setTimeout(type, typingDelay);
+        setTimeout(writeText, typingDelay);
     }
 
     else
     {
         cursorSpan.classList.remove("typing");
-        setTimeout(erase, newTextDelay);
+        setTimeout(deleteText, newTextDelay);
     }
 }
 
-function erase() 
+function deleteText() 
 {
     if (charIndex > 0) 
     {
         if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
         typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
         charIndex--;
-        setTimeout(erase, erasingDelay);
+        setTimeout(deleteText, erasingDelay);
     }
 
     else
@@ -40,11 +40,11 @@ function erase()
             textArrayIndex = 0;
         }
 
-        setTimeout(type, typingDelay + 1100);
+        setTimeout(writeText, typingDelay + 1100);
     }
 }
 
 document.addEventListener("DOMContentLoaded", function name() 
 {
-    if (textArray.length) setTimeout(type, newTextDelay + 250); 
+    if (textArray.length) setTimeout(writeText, newTextDelay + 250); 
 });
